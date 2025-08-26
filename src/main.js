d8ba75b1ae96ba1472c770e6c2ac1c7848752ba1
Josett Castillo <josettcastillo@gmail.com>
2025-08-26 10:55:48 -0600

    Hice mi codigo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
-  //aqui va tu código
+export function addTwoNumbers(x , y){
+  console.log(a + b)
+    return x + y 
 }
+    console.log(2, 3)
+    console.log(-2, -3)
+    console.log(13, +26)
+    console.log(-10, 30)
+ 
+addTwoNumbers();
