c3047a5e08d05f8c13be012e9e9f6c4cb237ac34
Josett Castillo <josettcastillo@gmail.com>
2025-08-26 11:41:02 -0600

    Ya me permite imprimir números pero aún no me sale la suma

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ export function addTwoNumbers(x , y){
   console.log(a + b)
     return x + y 
 }
-    console.log(2, 3)
-    console.log(-2, -3)
-    console.log(13, +26)
-    console.log(-10, 30)
+    console.log(2, 3);
+    console.log(-2, -3);
+    console.log(13, 26);
+    console.log(-10, 30);
  
-addTwoNumbers();
