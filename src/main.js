e120958419d8e4120c411e60d6d46fad853781fe
Josett Castillo <josettcastillo@gmail.com>
2025-08-26 17:50:49 -0600

    Ya logré hacer correctamente mi calculadora, ya muestra los resultados en una ventana también y funciona correctamente (:

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Este es mi primera actividad, funciona y pasa el test
 
 export function addTwoNumbers(x , y){
   console.log(x + y)
@@ -7,30 +8,46 @@ export function addTwoNumbers(x , y){
     console.log(addTwoNumbers(-2, -3));
     console.log(addTwoNumbers(13, 26));
     console.log(addTwoNumbers(-10, 30));
+
+
  
-const option = +prompt("Selecciona una opción\n 1. Sumar\n 2. Restar\n 3. Multiplicar\n 4. Dividir");
+    // Apartado para realizar la calculadora sencilla
+
+    const suma = function (x, y){
+        return x + y
+    }
+    const resta = function (x, y){
+        return x - y
+    }
+    const multiplicacion = function (x, y){
+        return x * y
+    }
+    const division = function (x, y){
+        return x / y
+    }
+
+const option = +prompt("Seleccione una opción\n 1. Sumar\n 2. Restar\n 3. Multiplicar\n 4. Dividir");
 switch(option){
     case 1:
-        const num1 = +prompt("Ingresa un número");
-        const num2 = +prompt("Ingresa otro número")
-        const suma = (num1 + num2);
-        console.log = (`Resultado: ${suma}`)
+        const num1 = +prompt("Ingrese el primer número");
+        const num2 = +prompt("Ahora ingrese el segundo número");
+        alert("Resultado: " + suma(num1, num2));
         break;
     case 2:
-        const numero1= +prompt("Ingresa un número");
-        const numero2= +prompt("Ingresa otro número");
-        console.log(Resta(numero1, numero2));
+        const numero1= +prompt("Ingrese el primer número");
+        const numero2= +prompt("Ahora ingrese el segundo número");
+        alert("Resultado: " + resta(numero1, numero2));
         break;
     case 3:
-        const nume1= +prompt("Ingresa un número");
-        const nume2= +prompt("Ingresa otro número");
-        console.log(Multiplicacion(nume1, nume2));
+        const nume1= +prompt("Ingrese el primer número");
+        const nume2= +prompt("Ahora ingrese el otro número");
+        alert("Resultado: " + multiplicacion(nume1, nume2));
         break;
     case 4:
-        const numb1= +prompt("Ingresa un número");
-        const numb2= +prompt("Ingresa otro número");
-        console.log(Multiplicacion(numb1, numb2));
+        const numb1= +prompt("Ingrese un número");
+        const numb2= +prompt("Ahora ingrese otro número");
+        alert("Resultado: " + division(numb1, numb2));
         break;
     default:
-        alert("Debes ingresar una opción válida, en número");
+        alert("Ingrese un valor válido en número por favor");
 } 
