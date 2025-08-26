9f107f3e73a1afd141cc9aa6e953f3e04fef5034
Josett Castillo <josettcastillo@gmail.com>
2025-08-26 17:01:44 -0600

    Ya me sale la ventana pero aún no logro que imprima el resultado

--- src/main.js
+++ src/main.js
@@ -8,3 +8,29 @@ export function addTwoNumbers(x , y){
     console.log(addTwoNumbers(13, 26));
     console.log(addTwoNumbers(-10, 30));
  
+const option = +prompt("Selecciona una opción\n 1. Sumar\n 2. Restar\n 3. Multiplicar\n 4. Dividir");
+switch(option){
+    case 1:
+        const num1 = +prompt("Ingresa un número");
+        const num2 = +prompt("Ingresa otro número")
+        const suma = (num1 + num2);
+        console.log = (`Resultado: ${suma}`)
+        break;
+    case 2:
+        const numero1= +prompt("Ingresa un número");
+        const numero2= +prompt("Ingresa otro número");
+        console.log(Resta(numero1, numero2));
+        break;
+    case 3:
+        const nume1= +prompt("Ingresa un número");
+        const nume2= +prompt("Ingresa otro número");
+        console.log(Multiplicacion(nume1, nume2));
+        break;
+    case 4:
+        const numb1= +prompt("Ingresa un número");
+        const numb2= +prompt("Ingresa otro número");
+        console.log(Multiplicacion(numb1, numb2));
+        break;
+    default:
+        alert("Debes ingresar una opción válida, en número");
+} 
